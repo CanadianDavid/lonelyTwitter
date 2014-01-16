5466c7342bfcdab18b14c5234bfe50c3938c033e
CanadianDavid <dveyee@hotmail.com>
2014-01-16 15:51:01 -0700

    Add NormalTweetModel

---
+++ src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
@@ -0,0 +1,21 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class NormalTweetModel extends LonelyTweetModel{
+    public NormalTweetModel(String text){
+        super(text);
+    }
+
+    @Override
+    public Date getTimestamp(){
+        return null;
+    }
+
+    @Override
+    public Boolean getPriority(){
+        return false;
+    }
+
+}
