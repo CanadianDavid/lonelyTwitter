c11971cc536b2d22f25fc284d2748756fef8c29f
CanadianDavid <dveyee@hotmail.com>
2014-01-16 16:45:20 -0700

    Refactor AbstractTweetModel and addTweet method to TweetController.

--- src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
@@ -3,18 +3,18 @@ package ca.ualberta.cs.lonelytwitter;
 import java.util.Date;
 
 
-public class NormalTweetModel extends LonelyTweetModel{
-    public NormalTweetModel(String text){
+public class NormalTweetModel extends AbstractTweetModel {
+    public NormalTweetModel(String text) {
         super(text);
     }
 
     @Override
-    public Date getTimestamp(){
+    public Date getTimestamp() {
         return null;
     }
 
     @Override
-    public boolean isImportant(){
+    public boolean isImportant() {
         return false;
     }
 
--- src/ca/ualberta/cs/lonelytwitter/TweetListModel.java
+++ src/ca/ualberta/cs/lonelytwitter/TweetListModel.java
@@ -2,7 +2,18 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
 
-public class TweetListModel{
-    private ArrayList<LonelyTweetModel> tweetList;
-    
+public class TweetListModel {
+    private static ArrayList<AbstractTweetModel> tweetList;
+    public TweetListModel() {
+        super();
+        tweetList = new ArrayList<AbstractTweetModel>();
+    }
+
+    public ArrayList<AbstractTweetModel> getTweetList() {
+        return tweetList;
+    }
+
+    public void setTweetList(ArrayList<AbstractTweetModel> tweetList) {
+        TweetListModel.tweetList = tweetList;
+    }
 }
